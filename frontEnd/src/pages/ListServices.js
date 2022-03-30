e10a055c4c33b11c6ffe35d07f9591b494e83924
repplayy <mikaelslopesit@gmail.com>
2022-03-30 10:07:01 -0300

    Primeira versão tela home, feito flatlist. Adicionado imagens para uso na home e criado navegação entre telas

---
+++ frontEnd/src/pages/ListServices.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, View } from 'react-native';
+
+
+
+export default function ListServices(navigation) {
+
+return(
+<View>
+    <Text>Ok está funcionando</Text>
+</View>
+
+
+);
+
+}
