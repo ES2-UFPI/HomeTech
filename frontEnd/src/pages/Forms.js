c2793af89e6c89f6bc547af76756dede494332bf
repplayy <mikaelslopesit@gmail.com>
2022-03-31 09:46:31 -0300

    formulário iniciado

--- frontEnd/src/pages/Forms.js
+++ frontEnd/src/pages/Forms.js
@@ -1,16 +1,84 @@
+import { Formik } from 'formik';
 import React from 'react';
-import { StyleSheet, Text, View, Image,FlatList,TouchableOpacity, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View,TextInput ,Image,FlatList,TouchableOpacity, SafeAreaView } from 'react-native';
 
 
 
 export default function Forms(navigation) {
 
     return(
+        <Formik>
         <View>
-            <Text>Ok está funcionando</Text>
+             <View>
+                 <Text>Computar Desktop </Text>
+                    <Text style={{ marginTop: 20 }}>Qual marca/modelo produto?</Text>
+                    <View style={styles.input}>
+                      <TextInput
+                        style={{ marginLeft: 10 }}
+                
+                        
+                    
+                      />
+                    </View>
+                  </View>
+                  <View>
+                    <Text style={{ marginTop: 20 }}>O problema está coberto de garantia ?</Text>
+                    <View style={styles.input}>
+                      <TextInput
+                        style={{ marginLeft: 10 }}
+                
+                        
+                    
+                      />
+                    </View>
+                  </View>
+                  <View>
+                    <Text style={{ marginTop: 20 }}>Qual problema encontra ?</Text>
+                    <View style={styles.input}>
+                      <TextInput
+                        style={{ marginLeft: 10 }}
+                
+                        
+                    
+                      />
+                    </View>
+                  </View>
+                  <View>
+                    <Text style={{ marginTop: 20 }}>informações adicionais</Text>
+                    <View style={styles.input}>
+                      <TextInput
+                        style={{ marginLeft: 10 }}
+                
+                        
+                    
+                      />
+                    </View>
+                  </View>
+                 
         </View>
+        </Formik>
         
-        
+
+
+
+       
+
         );
+      
+       
+
+       
 
 }
+
+const styles = StyleSheet.create({
+
+    input: {
+        borderWidth: 1,
+        width: "100%",
+        height: 24,
+        borderRadius: 15,
+        justifyContent: "center",
+      },
+
+          });
